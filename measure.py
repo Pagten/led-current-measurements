ed05688304cc7130cac102694fdea6be2b2ea5a8
Pieter Agten <pieter.agten@gmail.com>
2019-12-22 14:48:23 +0000

    Add support for varying voltage setpoint

--- measure.py
+++ measure.py
@@ -11,6 +11,7 @@ from dataclasses import dataclass
 from lib.koradserial import KoradSerial
 from apa102_pi.driver import apa102
 
+VOLTAGE_ABSOLUTE_MAX = 5.5
 COLOR_BITSHIFTS = [16, 8, 0] # RGB
 
 class ColorRange(ABC):
@@ -81,6 +82,27 @@ class Context:
         self.settle_time = settle_time
         self.current_offset = current_offset
 
+class RangeInc:
+    def __init__(self, start, stop, step):
+        if stop < start:
+            raise ValueError("Invalid range: stop must be greater than or equal to start")
+        if float(step) <= 0.0:
+            raise ValueError("step must be greater than 0")
+
+        self.start = start
+        self.stop = stop
+        self.step = step
+
+    def __iter__(self):
+        value = self.start
+        while (value < self.stop):
+            yield value
+            value += self.step
+        yield self.stop
+
+    def __len__(self):
+        return int((self.stop - self.start) // self.step) + 1
+
 def get_red(rgb_color):
     return int((rgb_color & 0xFF0000) >> 16)
   
@@ -99,7 +121,10 @@ def set_strip_color(strip, num_leds, red, green, blue, global_brightness):
     # Program strip
     strip.show()
 
-def do_measurement(ctx, brightness, red, green, blue):
+def do_measurement(ctx, voltage_setpoint, brightness, red, green, blue):
+    # Set voltage
+    ctx.psu_channel.voltage = voltage_setpoint
+
     # Set strip color
     set_strip_color(ctx.strip, ctx.num_leds, red, green, blue, brightness)
     
@@ -114,14 +139,14 @@ def do_measurement(ctx, brightness, red, green, blue):
     ctx.csv_writer.writerow([brightness, red, green, blue, voltage, current])
     return (voltage, current)
 
-def run_measurement_iteration(ctx, brightness, rgb_color, i, nb_iterations, start_time):
+def run_measurement_iteration(ctx, voltage_setpoint, brightness, rgb_color, i, nb_iterations, start_time):
     # Calculate RGB color
     red = get_red(rgb_color)
     green = get_green(rgb_color)
     blue = get_blue(rgb_color)
 
     # Run measurement
-    (voltage, current) = do_measurement(ctx, brightness, red, green, blue)
+    (measured_voltage, measured_current) = do_measurement(ctx, voltage_setpoint, brightness, red, green, blue)
 
     # Progress calculations
     progress = i / nb_iterations * 100
@@ -132,27 +157,29 @@ def run_measurement_iteration(ctx, brightness, rgb_color, i, nb_iterations, star
     minutes_remaining, seconds_remaining = divmod(remainder, 60)
 
     # Print status
-    ctx.stdscr.addstr(2, 0, f'Color: R{red:<3} G{green:<3} B{blue:<3}\t'
-                        f'Brightness: {brightness:2}/31')
-    ctx.stdscr.addstr(3, 0, f'Voltage: {voltage:4.2f} V\t\t'
-                        f'Current: {current:5.2f} mA')
-    ctx.stdscr.addstr(4, 0, f'Progress: {progress:6.2f}%\t'
-                        f'Time remaining: {int(hours_remaining):2}:{int(minutes_remaining):02}:{int(seconds_remaining):02}')
+    ctx.stdscr.addstr(2, 0, f'Voltage setpoint: {voltage_setpoint:4.2f} V')
+    ctx.stdscr.addstr(3, 0, f'Color: R{red:<3} G{green:<3} B{blue:<3}\t'
+                            f'Brightness: {brightness:2}/31')
+    ctx.stdscr.addstr(4, 0, f'Voltage: {measured_voltage:4.2f} V\t\t'
+                            f'Current: {measured_current:5.2f} mA')
+    ctx.stdscr.addstr(5, 0, f'Progress: {progress:6.2f}%\t'
+                            f'Time remaining: {int(hours_remaining):2}:{int(minutes_remaining):02}:{int(seconds_remaining):02}')
     ctx.stdscr.refresh()
 
-def run_measurements(ctx, mode, brightness_range, value_range):
+def run_measurements(ctx, mode, brightness_range, value_range, voltage_range):
     # Write header
     ctx.csv_writer.writerow(['Brightness (31)', 'Red (255)', 'Green (255)', 'Blue (255)', 'Voltage (V)', 'Current (mA)'])
 
     i = 0
     color_range = mode.color_range(value_range) 
-    nb_iterations = len(brightness_range) * len(color_range)
+    nb_iterations = len(voltage_range) * len(brightness_range) * len(color_range)
     start_time = datetime.datetime.now()
-    for cur_brightness in brightness_range:
-        for rgb_color in color_range:
-            i += 1
-            run_measurement_iteration(ctx, cur_brightness, rgb_color, i, nb_iterations, start_time)
-                    
+    for voltage in voltage_range:
+        for brightness in brightness_range:
+            for rgb_color in color_range:
+                i += 1
+                run_measurement_iteration(ctx, voltage, brightness, rgb_color, i, nb_iterations, start_time)
+
 def run(args, stdscr):
     mode_str = args.mode
     psu_port = args.psu_port
@@ -163,9 +190,13 @@ def run(args, stdscr):
     current_offset = int(args.current_offset)
     
     num_leds = int(args.num_leds)
-    brightness_range = range(int(args.min_brightness), int(args.max_brightness) + 1)
-    value_range = range(int(args.min_value), int(args.max_value) + 1)
-    settle_time = int(args.settle_time) / 1000.0
+    brightness_range = RangeInc(int(args.min_brightness), int(args.max_brightness), int(args.brightness_step))
+    value_range = RangeInc(int(args.min_value), int(args.max_value), int(args.value_step))
+    voltage_range = RangeInc(float(args.min_voltage), float(args.max_voltage), float(args.voltage_step))
+    settle_time = int(args.settle_time_ms) / 1000.0
+
+    if float(args.max_voltage) > VOLTAGE_ABSOLUTE_MAX:
+        raise ValueError("Max voltage out of range")
 
     if mode_str.upper() not in map(lambda enum: enum.name, list(Mode)):
         raise ValueError("Invalid mode specified")
@@ -185,7 +216,7 @@ def run(args, stdscr):
                 csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
                 psu_channel = power_supply.channels[psu_channel_no]
                 ctx = Context(stdscr, csv_writer, strip, psu_channel, num_leds, settle_time, current_offset)
-                run_measurements(ctx, mode, brightness_range, value_range)
+                run_measurements(ctx, mode, brightness_range, value_range, voltage_range)
         finally:
             strip.clear_strip()
             strip.cleanup()
@@ -202,9 +233,14 @@ def main():
     arg_parser.add_argument("--num-leds", dest='num_leds', help="number of LEDs in strip", default=20)
     arg_parser.add_argument("--min-brightness", dest='min_brightness', help="min global brightness to set", default=0)
     arg_parser.add_argument("--max-brightness", dest='max_brightness', help="max global brightness to set", default=31)
+    arg_parser.add_argument("--brightness-step", dest='brightness_step', help="global brightness step size", default=1)
     arg_parser.add_argument("--min-value", dest='min_value', help="min LED value to set", default=0)
     arg_parser.add_argument("--max-value", dest='max_value', help="max LED value to set", default=255)
-    arg_parser.add_argument("--settle-time", dest='settle_time', help="number of milliseconds to wait between each measurement for the current to settle", default=100)
+    arg_parser.add_argument("--value-step", dest='value_step', help="LED value step size", default=1)
+    arg_parser.add_argument("--min-voltage", dest='min_voltage', help="min voltage to set", default=5)
+    arg_parser.add_argument("--max-voltage", dest='max_voltage', help="max voltage to set", default=5)
+    arg_parser.add_argument("--voltage-step", dest='voltage_step', help="voltage step size", default=0.25)
+    arg_parser.add_argument("--settle-time", dest='settle_time_ms', help="number of milliseconds to wait between each measurement for the current to settle", default=100)
     arg_parser.add_argument("--current-offset", dest='current_offset', help="number of mA to deduct from the current measurements", default=0)
     arg_parser.add_argument("--force", action='store_true', help="don't abort if output-file already exists")
     args = arg_parser.parse_args()
